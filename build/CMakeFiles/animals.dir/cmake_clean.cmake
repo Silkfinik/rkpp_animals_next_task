35150d2807122cfa07030daba170b568c9290d2d
Silkfinik <wottankiazzaa@gmail.com>
2024-03-10 15:19:33 +0300

    15:19 10.03.2024

---
+++ build/CMakeFiles/animals.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/animals.dir/main.cpp.obj"
+  "CMakeFiles/animals.dir/main.cpp.obj.d"
+  "animals.exe"
+  "animals.exe.manifest"
+  "animals.pdb"
+  "libanimals.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/animals.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: D:/C++/VSCodeProjects/cpp/rkpp_animals
+# Install script for directory: D:/Programming/cpp/rkpp_animals
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "D:/C++/VSCodeProjects/cpp/rkpp_animals/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "D:/Programming/cpp/rkpp_animals/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
